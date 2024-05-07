b3fca8860fc33826cd509f135b41181cc54bdd20
Genesis McGruder <genesismcgruder@gmail.com>
2024-05-07 12:58:32 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate",5, "M","brown", "kind",1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy",8, "M", "purple", "high-energy",1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "orange", "sleepy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "black", "militant",0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 5, "female", "red", "fun", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 9, "male", "green", "ornery", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 2, "female", "grey", "youthful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 6, "male", "white", "shy", 1);
